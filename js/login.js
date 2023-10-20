7f0752bb50169c6c967f788e5e05407327d329ba
bernardo.mota <bernardo16ramos@gmail.com>
2023-10-20 13:09:13 -0300

    finalizado

--- js/login.js
+++ js/login.js
@@ -1,4 +1,24 @@
-// login.js
-function showMessage() {
-    alert("Olá, mundo!");
-}
+function entrar(){
+
+    
+    const email = document.getElementById('email')?.value;
+    const senha = document.getElementById('senha')?.value;
+    
+
+    let accounts = localStorage.getItem('accounts') ? localStorage.getItem('accounts') : [];
+
+    console.log(nome)
+
+    console.log(obj)
+
+    // if(nome == "admin" && senha == "admin" && email == "admin@gmail.com" && data == "admin"){
+    //     location.href = "paginainicial.html";
+    //     alert('sucesso');
+    // }else{
+    //     alert('usuario ou senha incorretos');
+    // }
+}    
+
+localStorage.setItem("nome", "email", "senha", "data");   
+
+   
--- js/registro.js
+++ js/registro.js
@@ -1,27 +1,35 @@
-document.addEventListener('DOMContentLoaded', function () {
-    
-    let form = document.getElementById('registrationForm');
+    function enviar(){
 
-    
-    form.addEventListener('submit', function (event) {
-        event.preventDefault();
+        const nome = document.getElementById('nome').value;
+        const email = document.getElementById('email').value;
+        const senha = document.getElementById('senha').value;
+        const data = document.getElementById('data').value;
 
-        
-        let nome = document.getElementById('nome').value;
-        let email = document.getElementById('email').value;
-        let senha = document.getElementById('senha').value;
-        let data = document.getElementById('data').value;
+        let accounts = localStorage.getItem('accounts') ? localStorage.getItem('accounts') : [];
 
-        
-        let usuario = {
+        console.log(nome)
+
+        let account = {
             nome: nome,
             email: email,
-            senha: senha
-        };
+            senha: senha,
+            data: data 
+        }
+
+        let newAccounts = accounts.push(account)
+
+        console.log(newAccounts)
+
+        console.log(obj)
+
+        // if(nome == "admin" && senha == "admin" && email == "admin@gmail.com" && data == "admin"){
+        //     location.href = "paginainicial.html";
+        //     alert('sucesso');
+        // }else{
+        //     alert('usuario ou senha incorretos');
+        // }
+    }    
+    
+    localStorage.setItem("nome", "email", "senha", "data");   
 
        
-        console.log('Informações do usuário:', usuario);
-
-       
-    });
-});
